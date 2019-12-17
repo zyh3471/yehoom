50362fa96bf7df36aeec4d7facbf22b0c412a6ac
Yehoom <zjzzhou@ucdavis.edu>
2019-12-17 20:37:38 +0000

    finish

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,10 +3,6 @@ class QuotesController < ApplicationController
        @quote = Quote.order("RANDOM()").first
     end
 
-    def new
-        @quote = Quote.new
-    end
-
     def create
         @quote = Quote.create(quote_params)
         if @quote.invalid?
@@ -14,7 +10,7 @@ class QuotesController < ApplicationController
         end
         redirect_to root_path
     end
-    
+
     def about
     end
 
